24cde88ba37dba6a8cc9a22e8577b957f707c8a6
daniel_urse <daniel.urse@hotmail.com>
2019-04-24 18:03:00 +0300

    update

--- src/app/routes/home/components/home/home.component.ts
+++ src/app/routes/home/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,72 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.mouseFloat();
+    this.viewScroll();
+  }
+
+  viewScroll() {
+    let aboutImages = $('.about-images div');
+    let aboutImage1 = $('.about-image-layer1');
+    let aboutImage2 = $('.about-image-layer2');
+
+    let answersImages = $('.answers-images div');
+    let answersImage1 = $('.answers-image-layer1');
+    let answersImage2 = $('.answers-image-layer2');
+
+    let ideasImages = $('.ideas-images div');
+    let ideasImage1 = $('.ideas-image-layer1');
+    let ideasImage2 = $('.ideas-image-layer2');
+    // console.log(scroll);
+    $(function () {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+
+        if (scroll >= 700 && scroll <= 1600) {
+          aboutImages[0].style.animation = ".66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          aboutImages[1].style.animation = " .66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          aboutImages[2].style.animation = ".66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          aboutImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+          aboutImage2[0].style.animation = ".5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        } else if(scroll >1500 && scroll <=2000){
+          answersImages[0].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          answersImages[1].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          answersImages[2].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          answersImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+          answersImage2[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        } else if(scroll >1800 && scroll <=2600){
+          ideasImages[0].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          ideasImages[1].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          ideasImages[2].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+          ideasImages[3].style.animation = "2s 1s forwards infinite bob-up-and-down cubic-bezier(.71, .01, 0, .9)";
+          ideasImages[3].style.opacity = '1';
+          ideasImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+          ideasImage2[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        }
+      });
+      let scroll = $(window).scrollTop();
+
+      if (scroll >= 700 && scroll <= 1600) {
+        aboutImages[0].style.animation = ".66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        aboutImages[1].style.animation = " .66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        aboutImages[2].style.animation = ".66667s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        aboutImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        aboutImage2[0].style.animation = ".5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+      } else if(scroll >1500 && scroll <=2000){
+        answersImages[0].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        answersImages[1].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        answersImages[2].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        answersImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        answersImage2[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+      } else if(scroll >1800 && scroll <=2600){
+        ideasImages[0].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        ideasImages[1].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        ideasImages[2].style.animation = "1s .3s forwards slide-downwards cubic-bezier(.71, .01, 0, .9)";
+        ideasImages[3].style.animation = "2s 1s forwards infinite bob-up-and-down cubic-bezier(.71, .01, 0, .9)";
+        ideasImages[3].style.opacity = '1';
+        ideasImage1[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+        ideasImage2[0].style.animation = " .5s .2s forwards slide-upwards cubic-bezier(.71, .01, .38, 1.01)";
+      }
+    });
   }
 
   mouseFloat() {
@@ -26,16 +93,16 @@ export class HomeComponent implements OnInit {
       _y: 0,
       x: 0,
       y: 0,
-      updatePosition: function(event) {
+      updatePosition: function (event) {
         let e = event || window.event;
         this.x = e.clientX - this._x;
         this.y = (e.clientY - this._y) * -1;
       },
-      setOrigin: function(e) {
+      setOrigin: function (e) {
         this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
         this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
       },
-      show: function() {
+      show: function () {
         return '(' + this.x + ', ' + this.y + ')';
       }
     };
@@ -47,22 +114,22 @@ export class HomeComponent implements OnInit {
 
     let counter = 0;
     let refreshRate = 10;
-    let isTimeToUpdate = function() {
+    let isTimeToUpdate = function () {
       return counter++ % refreshRate === 0;
     };
 
     //----------------------------------------------------
 
-    let onMouseEnterHandler = function(event) {
+    let onMouseEnterHandler = function (event) {
       update(event);
     };
 
-    let onMouseLeaveHandler = function() {
+    let onMouseLeaveHandler = function () {
       // @ts-ignore
       inner.style = '';
     };
 
-    let onMouseMoveHandler = function(event) {
+    let onMouseMoveHandler = function (event) {
       if (isTimeToUpdate()) {
         update(event);
       }
@@ -70,7 +137,7 @@ export class HomeComponent implements OnInit {
 
     //----------------------------------------------------
 
-    let update = function(event) {
+    let update = function (event) {
       mouse.updatePosition(event);
       // console.log(mouse.x + ' ' + mouse.y);
       // console.log(Math.abs(mouse.x / window.innerWidth));
@@ -81,7 +148,7 @@ export class HomeComponent implements OnInit {
       );
     };
 
-    let updateTransformStyle = function(x, y, styleX) {
+    let updateTransformStyle = function (x, y, styleX) {
       let style = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
       inner.style.transform = style;
       inner.style.webkitTransform = style;
