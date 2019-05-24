caaec8a40540e460a6ea55c541762759270f47b0
daniel_urse <daniel.urse@hotmail.com>
2019-05-24 18:51:17 +0300

    update categories stepp

--- src/app/routes/ask/components/categories/categories.component.ts
+++ src/app/routes/ask/components/categories/categories.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatStepper} from "@angular/material";
 
 @Component({
   selector: 'app-categories',
@@ -7,31 +8,41 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
-  firstFormGroup: FormGroup;
+  questionForm: FormGroup;
   secondFormGroup: FormGroup;
+  categorySelectedId: number;
   categorySelected: boolean;
 
   categoriesList = [
-    {title: 'AUDIO', class: 'fas fa-headphones'},
+    {title: 'A.I.', class: 'fas fa-robot'},
     {title: 'ANALYTICS', class: 'fas fa-chart-pie'},
     {title: 'ANIMALS', class: 'fas fa-dragon'},
+    {title: 'AUDIO', class: 'fas fa-headphones'},
     {title: 'AUTOMOTIVE', class: 'fas fa-car'},
     {title: 'BUILDINGS', class: 'fas fa-building'},
-    {title: 'NETWORK', class: 'fas fa-sitemap'},
-    {title: 'A.I.', class: 'fas fa-robot'},
     {title: 'CODING', class: 'fas fa-code'},
-    {title: 'DATABASE', class: 'fas fa-database'},
     {title: 'COMPUTERS', class: 'fas fa-laptop'},
-    ];
+    {title: 'DATABASE', class: 'fas fa-database'},
+    {title: 'DESIGN', class: 'fas fa-palette'},
+    {title: 'NETWORK', class: 'fas fa-sitemap'},
+    {title: 'SCHOOL', class: 'fas fa-user-graduate'},
+  ];
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.questionForm = this._formBuilder.group({
+      question: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+  }
+
+  goForward(stepper: MatStepper) {
+    setTimeout(() => {
+      stepper.next();
+    }, 0)
   }
 }
