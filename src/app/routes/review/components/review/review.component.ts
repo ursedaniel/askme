5ae813be4ff818581e1be959dfc99b4027e50213
daniel_urse <daniel.urse@hotmail.com>
2019-06-13 22:13:41 +0300

    update review

--- src/app/routes/review/components/review/review.component.ts
+++ src/app/routes/review/components/review/review.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../auth/services/auth.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-review',
@@ -12,8 +13,20 @@ export class ReviewComponent implements OnInit {
   user1: string;
   user2: string;
   currentUser: string;
+  stars = [
+    {value: 1, checked: false, hovered: false, class: 'far fa-star'},
+    {value: 2, checked: false, hovered: false, class: 'far fa-star'},
+    {value: 3, checked: false, hovered: false, class: 'far fa-star'},
+    {value: 4, checked: false, hovered: false, class: 'far fa-star'},
+    {value: 5, checked: false, hovered: false, class: 'far fa-star'},
+  ];
+  color: any;
+  checkedStar: number;
+  reviewForm: FormGroup;
+  review: string = '';
 
   constructor(
+    private _formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private auth: AuthService,
     private router: Router
@@ -24,6 +37,50 @@ export class ReviewComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.reviewForm = this._formBuilder.group({
+      review: ['', Validators.required]
+    });
   }
+
+  changeStyle($event, star, index) {
+    if ($event.type == 'mouseover') {
+      if (!star.hovered) {
+        for (let i = 0; i <= index; i++) {
+          this.stars[i].hovered = true;
+          if (index == 0)
+            this.stars[i].class = 'far fa-star star-hovered first';
+          else if (index == 1)
+            this.stars[i].class = 'far fa-star star-hovered second';
+          else if (index == 2)
+            this.stars[i].class = 'far fa-star star-hovered third';
+          else if (index == 3)
+            this.stars[i].class = 'far fa-star star-hovered fourth';
+          else if (index == 4)
+            this.stars[i].class = 'far fa-star star-hovered fifth';
+        }
+        for (let i = index + 1; i < this.stars.length; i++) {
+          this.stars[i].hovered = false;
+          this.stars[i].class = 'far fa-star'
+        }
+      }
+    } else {
+      if (!star.checked) {
+        for (let i = 0; i < this.stars.length; i++) {
+          this.stars[i].hovered = false;
+          this.stars[i].class = 'far fa-star'
+        }
+      }
+    }
+  }
+
+  chooseStar(star, i) {
+    for (let i = 0; i < this.stars.length; i++) {
+      this.stars[i].checked = false;
+    }
+    star.checked = !star.checked;
+    this.checkedStar = star.value;
+    this.changeStyle({type: 'mouseover'}, star, i);
+  }
+
 
 }
--- src/app/routes/review/review.module.ts
+++ src/app/routes/review/review.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReviewComponent } from './components/review/review.component';
 import {routing} from './routes/routes';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [ReviewComponent],
   imports: [
     CommonModule,
-    routing
-  ]
+    routing,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
+  providers: []
 })
 export class ReviewModule { }
